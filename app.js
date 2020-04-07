62f97297b3b65b82ee96a1eddd6a99e09690f736
Jason Vu <javu404@gmail.com>
2020-04-07 09:30:10 -0700

    4/7/2020

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
 //parseInt = converting non-integer number to an integer
 //parseFloat = converting non-float (without decimal) to float (with decimal)
+
 var app = new Vue({
 	el: '#app',
 	data: {
-		W_CSU_GPA: 0, // --Mapped To--> (v-model="W_CSU_GPA")
+		CSU_GPA: 0, // --Mapped To--> (v-model="CSU_GPA")
 		SAT_EBRW: 0, // --Mapped To--> (v-model="SAT_EBRW")
 		SAT_MATH: 0, // --Mapped To--> (v-model="SAT_MATH")
-		SAT: 0, 
+		SAT: 0,
+		// SERVICE_AREA: false, --Mapped To--> (v-model="SERVICE_AREA")
 		EI: null // --Mapped To--> {{ EI }}
-		//Service_Area: false, --Mapped To--> (v-model="Service_Area")
 	},
 	methods: {
 		calculate: function() {
 			var EI = 0;
-			W_CSU_GPA = parseInt(this.W_CSU_GPA * 800);
-			SAT_MATH = parseInt(this.SAT_MATH * 3);
-			SAT = parseInt(this.SAT_MATH + this.SAT_EBRW)/2;
-			EI += parseInt(this.SAT + this.W_CSU_GPA);
-			this.EI = Math.floor(EI);
+			var SAT = 0;
+			CSU_GPA = parseFloat(this.CSU_GPA) * parseInt(800);
+			SAT_MATH = parseInt(this.SAT_MATH) * parseInt(3);
+			SAT = parseInt(this.SAT_MATH) + parseInt(this.SAT_EBRW);
+			SAT = parseInt(SAT)/parseInt(2);
+			EI = parseInt(this.SAT) + parseFloat(this.CSU_GPA);
+			this.EI = parseInt(Math.floor(EI));
 		},
 	},
 })
-/* |Theoretical equation as expressed in Python|.
-
-GPA = float(input("Enter Weighted GPA: ")); --Mapped To--> (v-model="W_CSU_GPA")
-Location = input("Do you live near SJSU? (yes/no): "); --Mapped To--> (v-model="Service_Area")
-SAT_Math = int(input("Enter SAT Math score: ")); --Mapped To--> (v-model="SAT_MATH")
-SAT_EBRW = int(input("Enter SAT English score: ")); --Mapped To--> (v-model="SAT_EBRW")
-COE_TotalScore = int(((SAT_Math * 3) + (SAT_EBRW))/2);
-
-if (Location == "yes"):
-  GPA = float(GPA + 0.25);
-elif (Location == "no"):
-  GPA = GPA;
-else:
-  GPA = GPA;
-
-COE_ImpactionIndex = int(COE_TotalScore + (float(GPA) * 800));
-print("Eligbility Index: " + str(COE_ImpactionIndex));
-
-*/
 
 /* |Algorithm:|
 1) Weighted GPA is multiplied by 800 
-2) Multiply SAT-MATH by 3 and add it with SAT-ENGLISH
-3) Add SAT score with (GPA x 800)
-4) Voila! Your SJSU eligibility index score!
+2) Multiply SAT-MATH by 3 
+3) Add SAT-MATH with SAT-ENGLISH
+4) Divide SAT total score by 2
+5) Add SAT total score with CSU-weighted-GPA
+6) Voila! Your SJSU eligibility index score!
 */
