fdf12d6d2fd72c4ba37b6924f9e8baa88b10bd7f
Jason Vu <javu404@gmail.com>
2020-04-08 11:19:40 -0700

    4/8/20

--- app.js
+++ app.js
@@ -7,29 +7,24 @@ var app = new Vue({
 		CSU_GPA: 0, // --Mapped To--> (v-model="CSU_GPA")
 		SAT_EBRW: 0, // --Mapped To--> (v-model="SAT_EBRW")
 		SAT_MATH: 0, // --Mapped To--> (v-model="SAT_MATH")
-		SAT: 0,
-		// SERVICE_AREA: false, --Mapped To--> (v-model="SERVICE_AREA")
+		SERVICE_AREA: false, // --Mapped To--> (v-model="SERVICE_AREA")
 		EI: null // --Mapped To--> {{ EI }}
 	},
 	methods: {
 		calculate: function() {
 			var EI = 0;
-			var SAT = 0;
-			CSU_GPA = parseFloat(this.CSU_GPA) * parseInt(800);
-			SAT_MATH = parseInt(this.SAT_MATH) * parseInt(3);
-			SAT = parseInt(this.SAT_MATH) + parseInt(this.SAT_EBRW);
-			SAT = parseInt(SAT)/parseInt(2);
-			EI = parseInt(this.SAT) + parseFloat(this.CSU_GPA);
-			this.EI = parseInt(Math.floor(EI));
+			EI += ((parseInt(this.SAT_EBRW) + parseInt(this.SAT_MATH * 3))/2);
+			EI += parseFloat(this.CSU_GPA) * 800;
+			this.EI = EI;
 		},
 	},
 })
 
 /* |Algorithm:|
-1) Weighted GPA is multiplied by 800 
-2) Multiply SAT-MATH by 3 
-3) Add SAT-MATH with SAT-ENGLISH
-4) Divide SAT total score by 2
-5) Add SAT total score with CSU-weighted-GPA
-6) Voila! Your SJSU eligibility index score!
+1) Multiply SAT-MATH by 3 
+2) Add SAT-MATH with SAT-ENGLISH
+3) Divide SAT by 2
+4) Multiply CSU_GPA by 800
+4) Add SAT with CSU_GPA
+5) Voila! Your SJSU eligibility index score!
 */
