0ea439e40b37ffb559405cd07e4f0a09b86beba5
Jason Vu <javu404@gmail.com>
2020-04-24 14:36:03 -0700

    Updated 4/24/20

--- app.js
+++ app.js
@@ -7,16 +7,44 @@ var app = new Vue({
 		CSU_GPA: 0, // --Mapped To--> (v-model="CSU_GPA")
 		SAT_EBRW: 0, // --Mapped To--> (v-model="SAT_EBRW")
 		SAT_MATH: 0, // --Mapped To--> (v-model="SAT_MATH")
-		SERVICE_AREA: false, // --Mapped To--> (v-model="SERVICE_AREA")
+		SERVICE_AREA: 0, // --Mapped To--> (v-model="SERVICE_AREA")
+		STANDARD: 2950, 	
+		DECISION: true,
 		EI: null // --Mapped To--> {{ EI }}
 	},
 	methods: {
 		calculate: function() {
 			var EI = 0; //changes from "null" to 0 when "calculate" button is pressed
+			console.log("EI at initialization: " + EI); //console log at init
 			EI += Math.min(4.2, parseFloat(this.CSU_GPA)) * 800; //value must be less than or equal to 4.2
 			EI += (Math.min(800, parseInt(this.SAT_EBRW)) + Math.min(800, parseInt(this.SAT_MATH)) * 3)/2; //values must be less than or equal to 800
-			EI += this.SERVICE_AREA ? 200 : 0; //Adds 200 points to EI if box is ticked
+			if (Math.max(0, parseFloat(this.SERVICE_AREA)) >= 0 && Math.max(0, parseFloat(this.SERVICE_AREA) <= 20)) {
+				EI += 200; //will add 200 points to EI if greater than 0 and less than or equal to 20
+			} else {
+				EI = EI; //will remain the same if greater than 20
+			}
 			this.EI = EI; // renders output in {{ EI }}
+			var DECISION = true; 
+			while (DECISION == true) {
+				if (this.EI >= Math.min(2950, parseInt(this.STANDARD)) && this.EI <= Math.max(3750, parseInt(this.STANDARD))) {
+					DECISION = "You've met minimum requirements for undergraduate admission.";
+					this.DECISION = DECISION;
+					console.log(DECISION);
+				} else if (this.EI >= Math.min(3750, parseInt(this.STANDARD)) && (this.EI <= Math.max(4825, parseInt(this.STANDARD)))) {
+					DECISION = "You've exceeded minimum requirements for undergraduate admission.";
+					this.DECISION = DECISION;
+					console.log(DECISION);
+				} else if (this.EI > Math.min(4825, parseInt(this.STANDARD))) {
+					DECISION = "You've transcended minimum requirements for undergraduate admission.";
+					this.DECISION = DECISION;
+					console.log(DECISION);
+				} else {
+					DECISION = "You've not met minimum requirements for undergraduate admission";
+					this.DECISION = DECISION;
+					console.log(DECISION);			
+				}
+			}
+			console.log("Your SJSU EI is: " + EI); //console log at finish
 		},
 	},
 })
